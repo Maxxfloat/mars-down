f365b667f34e8dc0f3e645a009f9720500c1858b
Omid-Neshati <tchandnofame@gmail.com>
2022-11-26 17:38:15 +0330

    add note list page

---
+++ src/pages/NoteList/index.tsx
@@ -0,0 +1,94 @@
+import { useState, FC, useMemo } from "react";
+import { Row, Col, Stack, Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ReactSelect from "react-select";
+import { Note, Tag } from "../../types/form";
+import EditTagsModal from "./EditTagsModal";
+import NoteCard from "./NoteCard";
+
+const index: FC<{ availableTags: Tag[]; notes: Note[] }> = ({
+  availableTags,
+  notes,
+}) => {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [title, setTitle] = useState("");
+  const [editTagsModalIsOpen, setEditTagsModalIsOpen] = useState(false);
+
+  const filteredNotes = useMemo(() => {
+    return notes.filter((note) => {
+      return (
+        title === "" ||
+        (note.title.toLowerCase().includes(title.toLowerCase()) &&
+          (selectedTags.length === 0 ||
+            selectedTags.every((tag) =>
+              note.tags.some((noteTag) => noteTag.id === tag.id)
+            )))
+      );
+    });
+  }, [notes, selectedTags, title]);
+
+  return (
+    <>
+      <Row className="justify-content-end">
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to="/new">
+              <Button variant="primary">Create</Button>
+            </Link>
+            <Button
+              variant="outline-secondary"
+              onClick={() => setEditTagsModalIsOpen(true)}
+            >
+              Edit Tags
+            </Button>
+          </Stack>
+        </Col>
+      </Row>
+      <Form>
+        <Row className="mb-4">
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+                value={selectedTags.map((t) => ({
+                  label: t.label,
+                  value: t.id,
+                }))}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((t) => ({ label: t.label, id: t.value }))
+                  );
+                }}
+                isMulti
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+      <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
+        {filteredNotes.map((note) => (
+          <Col key={note.id}>
+            <NoteCard id={note.id} title={note.title} tags={note.tags} />
+          </Col>
+        ))}
+      </Row>
+      <EditTagsModal
+        availableTags={availableTags}
+        show={editTagsModalIsOpen}
+        handleClose={() => setEditTagsModalIsOpen(false)}
+      />
+    </>
+  );
+};
+
+export default index;
